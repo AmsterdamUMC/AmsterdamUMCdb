9a5999ee580a1fa2ac2117439c5ff8554f0c080a
Patrick Thoral <42101283+patrickthoral@users.noreply.github.com>
2021-05-23 01:47:21 +0200

    improves plotting of outlier histograms
    Updates the `amsterdamumcdb.outlier_histogram()` function.

--- amsterdamumcdb/plotting.py
+++ amsterdamumcdb/plotting.py
@@ -1,18 +1,25 @@
 import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
 import numpy as np
 
-def outliers_histogram(data, z_treshold=4.0, lower=None, upper=None, bins='auto'):
+
+def outliers_histogram(data, z_threshold=4.0, lower=None, upper=None, bins=None, binwidth=None, maxbins=None):
     """Return a pyplot histogram, where the upper and/or lower outliers are
-    binned together for a more even distribution plot.
+    binned together for a more even distribution plot. By default, the histogram will be created with bins that are on
+    boundaries aligned with the pyplot axis ticks.
 
     Arguments:
-        data -- dataframe to calculate the median absolute deviatan for
-        z_treshold -- number of standard deviations from the median to determine outliers
-        lower -- lower threshold for binning lower outliers together
-        upper -- upper threshold for binning higer outliers together
+        data -- dataframe to create the histogram for.
+        z_threshold (Optional) -- number of standard deviations from the median to determine outliers
+        lower (Optional) -- lower threshold for binning lower outliers together
+        upper (Optional) -- upper threshold for binning higher outliers together
+        bins (Optional) -- int or sequence or str, allows specifying the number of bins, the actual bins or a binning strategy
+            (see: https://matplotlib.org/stable/api/_as_gen/matplotlib.pyplot.hist.html)
+        binwidth (Optional) -- forces a specified size of the equally sized bins between the lower and upper threshold
+        maxbins -- the maximum number of equally sized bins to create
     """
     if (not lower) & (not upper):
-        lower, upper = calculate_z_range(data, z_treshold)
+        lower, upper = z_range(data, z_threshold)
 
     if not lower or lower < data.min():
         lower = data.min()
@@ -26,16 +33,23 @@ def outliers_histogram(data, z_treshold=4.0, lower=None, upper=None, bins='auto'
     else:
         upper_outliers = True
 
+    if not bins:
+        # if bins not defined, calculate number of bins
+
+        if not binwidth:
+            # if binwidth not defined, determine the bindwith based on
+            # the ticks pyplot will generate
+            binwidth = pretty_binwidth(lower, upper, maxbins)
+            bins = np.arange(round_base(lower, base=binwidth), round_base(upper, base=binwidth) + binwidth, binwidth)
+        else:
+            bins = np.arange(round_base(lower, base=binwidth), round_base(upper, base=binwidth) + binwidth, binwidth)
+
     n, bins, patches = plt.hist(data, range=(lower, upper), bins=bins)
-    # cm = plt.cm.get_cmap('RdYlBu_r')
-    # cm = plt.cm.get_cmap('Reds')
-    # cm = plt.cm.get_cmap('RdPu')
     cm = plt.cm.get_cmap('magma')
 
-    # To normalize your values
+    # To normalize the values
     col = (n - n.min()) / (n.max() - n.min())
     for c, p in zip(col, patches):
-        # plt.setp(p, 'facecolor', cm(c))
         plt.setp(p, 'facecolor', cm(0.15 + 0.6 * c))
 
     if lower_outliers:
@@ -57,26 +71,59 @@ def outliers_histogram(data, z_treshold=4.0, lower=None, upper=None, bins='auto'
 
 
 def mad(data):
-    """Return the median absolute deviation for the data set.
-
-    Arguments:
-        data -- the dataframe to calculate the median absolute deviation for
-    """
-    median = np.median(data)
-    diff = np.abs(data - median)
-    mad = np.median(diff)
-    return mad
-
-
-def calculate_z_range(data, z_thresh=3.5):
-    """Calculate the range for the dataset base on the specified Z threshold: the number of standard
-    deviations from the median.
+    """Return the median absolute deviation for the dataset.
 
     Arguments:
         data -- the dataframe to calculate the median absolute deviation for.
+    """
+    median = np.median(data)
+    diff = np.abs(data - median)
+    MAD = np.median(diff)
+    return MAD
+
+
+def z_range(data, z_thresh=4):
+    """Calculate the range for the dataset based on the specified Z threshold: the number of standard
+    deviations from the median.
+
+    Arguments:
+        data -- the dataframe to calculate the Z range for.
         z_thresh -- the Z threshold.
     """
     MAD = mad(data)
     median = np.median(data)
     const = z_thresh * MAD / 0.6745  # sd = 0.6745*MAD
     return (median - const, median + const)
+
+
+def round_base(x, base=.05):
+    """"rounds the value up to the nearest base"""
+    return round(base * round(float(x) / base))
+
+
+def pretty_binwidth(lower, upper, maxbins=None):
+    """uses the matplotlib.ticker.AutoLocator (for generating the ticks in the
+    plot), to determine the bins to use. By default, ticks will be placed on
+    positions that are multiples of the following sequence: [1, 2, 2.5, 5, 10].
+    Equally, for a nice display, the five bins will be generated between these
+    ticks.
+
+    Arguments:
+        lower -- the lower boundary of the dataset
+        upper -- the upper boundary of the dataset
+    """
+    locator = ticker.AutoLocator()
+    ticks = locator.tick_values(vmin=lower, vmax=upper)
+
+    if maxbins:
+        # determine binwidth based on maxbins
+        bins_per_tick = int(maxbins / len(ticks))
+        if bins_per_tick == 0:
+            bins_per_tick = 1
+    else:
+        # default: allow five bins between every tick
+        bins_per_tick = 5
+
+    binwidth = (ticks[1] - ticks[0]) / bins_per_tick
+
+    return binwidth
